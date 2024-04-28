8cd6ac5e7b0e7b3498ebcbc07afc7e6a20cde5c8
CrisDAndres <cdeandresgil@gmail.com>
2024-04-28 14:26:05 +0200

    last commit

--- agrotech.py
+++ agrotech.py
@@ -291,8 +291,8 @@ elif page == "Predictions":
     st.markdown("<p style='color: darkgreen; font-size: 36px; text-align: center;'>CropWise 🌱</p>", unsafe_allow_html=True)
     st.markdown("<p style='color: darkgreen; font-size: 24px; text-align: center;'>A crop recommendation platform using machine learning</p>", unsafe_allow_html=True)
     # download models files from google drive
-    url1 = "https://drive.google.com/uc?id=1TfydDkRqT2zJINmXc6RvrG7HOEuxkZgG/view?usp=sharing"
-    url2 = "https://drive.google.com/uc?id=1jRCQOX5_n6-Z-KtTZaCaDg-HNIneG6wN/view?usp=sharing"
+    url1 = "https://drive.google.com/uc?id=1TfydDkRqT2zJINmXc6RvrG7HOEuxkZgG"
+    url2 = "https://drive.google.com/uc?id=1jRCQOX5_n6-Z-KtTZaCaDg-HNIneG6wN"
     
     model_classif = "crop_RF.pkl"
     model_regr = "yield_RF.pkl"
